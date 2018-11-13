453f24bccfa27c01d1861cf7b9250ac9d86e4de1
Jasper van Rijbroek <jasper@jvar.nl>
2018-11-13 17:16:53 +0100

    Added theme color to the label model.

--- source/projects/main/lib/Model/SCR/LinksModel.php
+++ source/projects/main/lib/Model/SCR/LinksModel.php
@@ -76,4 +76,15 @@ class LinksModel extends ScrModel
             'persons' => $persons
         ];
     }
+
+    public function getPropertyTheme()
+    {
+        if (!isset($this->container['label-colors'])) {
+            $this->container['label-colors'] = json_decode(file_get_contents(__DIR__ . '/Label/SearchModel.json'), true);
+        }
+
+        return [
+            'color' => isset($this->container['label-colors']['theme']['color'][$this['_id']]) ? $this->container['label-colors']['theme']['color'][$this['_id']] : ''
+        ];
+    }
 }
