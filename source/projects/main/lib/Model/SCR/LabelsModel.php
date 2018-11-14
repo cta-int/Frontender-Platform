07ead49e7d0c6d13d84d61818d706ba92c05075a
Babs Gösgens <babsgosgens@gmail.com>
2018-11-14 10:29:24 +0100

    Change var names

--- source/projects/main/lib/Model/SCR/LabelsModel.php
+++ source/projects/main/lib/Model/SCR/LabelsModel.php
@@ -15,12 +15,14 @@ class LabelsModel extends ScrModel
 
     public function getPropertyTheme()
     {
-        if (!isset($this->container['label-colors'])) {
-            $this->container['label-colors'] = json_decode(file_get_contents(__DIR__ . '/Label/SearchModel.json'), true);
+        if (!isset($this->container['theme-color'])) {
+            $this->container['theme-color'] = json_decode(file_get_contents(__DIR__ . '/Label/SearchModel.json'), true);
         }
 
+        var_dump($this['type']);
+
         return [
-            'color' => isset($this->container['label-colors']['theme']['color'][$this['_id']]) ? $this->container['label-colors']['theme']['color'][$this['_id']] : ''
+            'color' => isset($this->container['theme-color'][$this['type']]['theme'][$this['_id']]) ? $this->container['theme-color']['theme']['theme-color'][$this['_id']] : ''
         ];
     }
 }
