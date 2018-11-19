9b4f46261ae2c5cb85e40c70a01da814c6927253
Babs Gösgens <babsgosgens@gmail.com>
2018-11-19 15:41:00 +0100

    Update source/projects/main/lib/Model/SCR/EventsModel.php

--- source/projects/main/lib/Model/SCR/EventsModel.php
+++ source/projects/main/lib/Model/SCR/EventsModel.php
@@ -93,6 +93,17 @@ class EventsModel extends ScrModel
         };
     }
 
+    public function getProjectOutputs()
+    {
+        // Linked content, type file
+    }
+
+    public function getProjectUpdates()
+    {
+        $_label = $this->getPropertyLabels('programme', true);
+        // Articles with programme label and of type 'blog'
+    }
+
     public function getPropertyRelated()
     {
         $linksModel = new LinksModel($this->container);
@@ -135,67 +146,81 @@ class EventsModel extends ScrModel
         return parent::getPropertyPath();
     }
 
-    private function _bindRegionId(&$event)
+    // private function _bindRegionId(&$event)
+    // {
+    //     // get all the regions.
+    //     $regions = [
+    //         '7729886' => 'purple',
+    //         '7729889' => 'red',
+    //         '7729885' => 'orange',
+    //         '9406051' => 'yellow',
+    //         '7729891' => 'gold',
+    //         '2363254' => 'blue'
+    //     ];
+    //     $ids = array_column($event['analysis']['geoname']['geonames'] ?? [], '_id');
+
+    //     $items = array_intersect($ids, array_keys($regions));
+    //     $event['region_id'] = array_shift($items);
+
+    //     if ($event['region_id']) {
+    //         $event['theme'] = $regions[$event['region_id']];
+    //     }
+    // }   
+    
+    public function getPropertyLabels(string $type='', bool $first=false)
     {
-        // get all the regions.
-        $regions = [
-            '7729886' => 'purple',
-            '7729889' => 'red',
-            '7729885' => 'orange',
-            '9406051' => 'yellow',
-            '7729891' => 'gold',
-            '2363254' => 'blue'
-        ];
-        $ids = array_column($event['analysis']['geoname']['geonames'] ?? [], '_id');
+        $_labels = $this->data['label'];
 
-        $items = array_intersect($ids, array_keys($regions));
-        $event['region_id'] = array_shift($items);
-
-        if ($event['region_id']) {
-            $event['theme'] = $regions[$event['region_id']];
+        if( $type != '' ) {
+            foreach( $_labels as $key => $value) {
+                if($value['type'] != $type) {
+                    unset($_labels[$key]);
+                }  
+            }
         }
+
+        if( $first ) {
+            // $_labels = array_slice($_labels, 0, 1);
+            $_labels = array_shift($_labels);
+        }
+
+        return $_labels;
     }
 
     public function getPropertyTheme()
     {
-        if (!isset($this->container['theme-color'])) {
-            $this->container['theme-color'] = json_decode(file_get_contents(__DIR__ . '/Label/SearchModel.json'), true);
-        }
+        $_label = $this->getPropertyLabels('strategy', true);
+        // @todo make this a model property, $this->config
+        $_config = json_decode(file_get_contents(__DIR__ . '/Label/SearchModel.json'), true);
+        $_theme = [];
+        
+        if( isset($_config[$_label['type']]['theme-color'][$_label['_id']]) ) {
+            $_theme['color'] = $_config[$_label['type']]['theme-color'][$_label['_id']];
+        } 
+        
+        return $_theme;
 
-        $themeColors = $this->container['theme-color'];
-        $color = '';
-        $label = false;
+        // if (!isset($this->container['theme-color'])) {
+        //     $this->container['theme-color'] = json_decode(file_get_contents(__DIR__ . '/Label/SearchModel.json'), true);
+        // }
 
-        if ($this['type'] === 'Project') {
-            $regions = [
-                '7729886' => 'purple',
-                '7729889' => 'red',
-                '7729885' => 'orange',
-                '9406051' => 'yellow',
-                '7729891' => 'gold',
-                '2363254' => 'blue'
-            ];
-            $ids = array_column($this['analysis']['geoname']['geonames'] ?? [], '_id');
+        // $themeColors = $this->container['theme-color'];
+        // $color = '';
 
-            $items = array_intersect($ids, array_keys($regions));
-            $event['region_id'] = array_shift($items);
+        // if (isset($this['link']['label'])) {
+        //     $labels = array_filter($this['link']['label'], function ($label) use ($themeColors) {
+        //         return isset($themeColors[$label['type']]['theme-color'][$label['_id']]);
+        //     });
 
-            if ($event['region_id']) {
-                $color = $regions[$event['region_id']];
-            }
-        } else if (isset($this['label']) && count($this['label'])) {
-            $labels = array_filter($this['label'], function ($label) use ($themeColors) {
-                return isset($themeColors[$label['type']]['theme-color'][$label['_id']]);
-            });
+        //     if (count($labels)) {
+        //         $color = array_shift($labels);
+        //         $color = isset($this->container['theme-color'][$color['type']]['theme-color'][$color['_id']]) ? $this->container['theme-color'][$color['type']]['theme-color'][$color['_id']] : '';
+        //     }
+        // }
 
-            $label = array_shift($labels);
-            $color = isset($this->container['theme-color'][$label['type']]['theme-color'][$label['_id']]) ? $this->container['theme-color'][$color['type']]['theme-color'][$label['_id']] : '';
-        }
-
-        return [
-            'color' => $color,
-            'label' => $label
-        ];
+        // return [
+        //     'color' => $color
+        // ];
     }
 
     public function getPropertyLeadImage()
