9f6e7fd381864600b36c7434bf460fed037b32e5
Jasper van Rijbroek <jasper@jvar.nl>
2018-11-28 13:09:39 +0100

    Updated health check.

--- health.php
+++ health.php
@@ -1,3 +1,44 @@
 <?php
 
+// Check for the env Variables.
+if (!isset($_ENV['MONGO_HOST'])) {
+    return false;
+}
+
+if (!isset($_ENV['MONGO_DB'])) {
+    return false;
+}
+
+if (!isset($_ENV['FEP_TOKEN_SECRET'])) {
+    return false;
+}
+
+if (!isset($_ENV['FEP_TOKEN_HEADER'])) {
+    return false;
+}
+
+if (!isset($_ENV['ENV'])) {
+    return false;
+}
+
+if (!isset($_ENV['CUSTOMER_KEY'])) {
+    return false;
+}
+
+if (!isset($_ENV['CUSTOMER_SECRET'])) {
+    return false;
+}
+
+if (!isset($_ENV['TOKEN'])) {
+    return false;
+}
+
+if (!isset($_ENV['TOKEN_SECRET'])) {
+    return false;
+}
+
+if (!isset($_ENV['SCR_TOKEN'])) {
+    return false;
+}
+
 return true;
