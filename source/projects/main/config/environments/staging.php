a568ae7a6f5c4dd5bf41725e0a9697249642dfbf
Babs Gösgens <babsgosgens@gmail.com>
2018-12-19 11:17:01 +0100

    Temporarily switch to production settings

--- source/projects/main/config/environments/staging.php
+++ source/projects/main/config/environments/staging.php
@@ -5,10 +5,20 @@
  * @link        http://www.dipity.eu
  */
 
-/* Staging */
-ini_set('display_errors', 0);
+// /* Staging */
+// ini_set('display_errors', 0);
+
+// return [
+//     'debug'      => 0,
+//     'caching'    => 0,
+// ];
+
+/* Development */
+ini_set('display_errors', 1);
 
 return [
-    'debug'      => 0,
-    'caching'    => 0,
+    'debug'             => 1,
+    'caching'           => 0,
+    'offline'           => 0,
+    'translation_debug' => 0,
 ];
