5182535331fb9e1908bd29776a6bbf2cf5eb7460
Jasper van Rijbroek <jasper@jvar.nl>
2019-08-30 11:47:24 +0200

    Only forward query statements in the translate url method.

--- source/projects/main/lib/Template/Helper/Url.php
+++ source/projects/main/lib/Template/Helper/Url.php
@@ -36,7 +36,8 @@ class Url extends Helper\Url
             // $params['page'] .= !empty($query) ? '?' . $query : '';
         }
 
-        return $router->route($params);
+        return $router->route($params)
+            ->withQuery($this->container['request']->getUri()->getQuery());
     }
 
     public function getTranslatedSlug($data, $locale)
