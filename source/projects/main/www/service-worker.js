af617f44050e29d5dfff3d769a45de9de9bac311
Leijten <jeroen@teemr.com>
2019-09-18 12:42:16 +0200

    Updated service worker

--- source/projects/main/www/service-worker.js
+++ source/projects/main/www/service-worker.js
@@ -1,32 +1,37 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
-
+// Basic Workbox configuration
 workbox.setConfig({
     debug: true
 });
 
+workbox.googleAnalytics.initialize();
+// End of basic Workbox configuration
+
+// Cache Google API urls
 workbox.routing.registerRoute(
-    // Cache JavaScript files.
+  /.*(?:googleapis|gstatic)\.com/,
+  new workbox.strategies.StaleWhileRevalidate(),
+);
+
+// Caching strategies for assets
+workbox.routing.registerRoute(
     /\.js$/,
     // Load from network first, if network fails used cached files
     new workbox.strategies.NetworkFirst()
 );
 
 workbox.routing.registerRoute(
-    // Cache CSS files.
     /\.css$/,
     // Use cache but update in the background.
     new workbox.strategies.StaleWhileRevalidate({
-        // Use a custom cache name.
         cacheName: 'css-cache',
     })
 );
 
 workbox.routing.registerRoute(
-    // Cache image files.
     /\.(?:png|jpg|jpeg|svg|gif)$/,
     // Use the cache if it's available.
     new workbox.strategies.CacheFirst({
-        // Use a custom cache name.
         cacheName: 'image-cache',
         plugins: [
             new workbox.expiration.Plugin({
