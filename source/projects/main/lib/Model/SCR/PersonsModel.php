d771f132aae2f15905d217c1f5b98c4761a4a211
Jasper van Rijbroek <jasper@jvar.nl>
2019-11-14 10:22:52 +0100

    Added publication property.

--- source/projects/main/lib/Model/SCR/PersonsModel.php
+++ source/projects/main/lib/Model/SCR/PersonsModel.php
@@ -11,44 +11,73 @@ namespace Frontender\Platform\Model\SCR;
 use Frontender\Platform\Model\Event\AttendeesModel;
 use Slim\Container;
 
-class PersonsModel extends ScrModel
-{
-    public function __construct(Container $container)
-    {
-        parent::__construct($container);
+class PersonsModel extends ScrModel {
+	public function __construct( Container $container ) {
+		parent::__construct( $container );
 
-        $this->getState()
-            ->insert('id', null, true)
-            ->insert('language', $this->container->language->get())
-            ->insert('limit', 20)
-	        ->insert('eventLimit', 20)
-	        ->insert('articleLimit', 20)
-            ->insert('skip');
-    }
+		$this->getState()
+		     ->insert( 'id', null, true )
+		     ->insert( 'language', $this->container->language->get() )
+		     ->insert( 'limit', 20 )
+		     ->insert( 'eventLimit', 20 )
+		     ->insert( 'articleLimit', 20 )
+		     ->insert( 'skip' );
+	}
 
-    public function getPropertyEvents() {
-    	$model = new \Frontender\Platform\Model\SCR\Event\SearchModel($this->container);
-    	$model->setState([
-    		'must' => [
-    			'type' => 'person',
-			    'id' => $this['_id']
-		    ],
-		    'limit' => $this->getState()->eventLimit
-	    ]);
-
-    	return $model->fetch();
-    }
-
-    public function getPropertyArticles() {
-    	$model = new \Frontender\Platform\Model\SCR\Article\SearchModel($this->container);
-		$model->setState([
-			'must' => [
-				'type' => 'person',
-				'id' => $this['_id']
+	public function getPropertyEvents() {
+		$model = new \Frontender\Platform\Model\SCR\Event\SearchModel( $this->container );
+		$model->setState( [
+			'must'  => [
+				[
+					'type' => 'person',
+					'id'   => $this['_id']
+				]
 			],
-			'limit' => $this->getState()->articleLimit
-		]);
+			'limit' => $this->getState()->eventLimit
+		] );
 
 		return $model->fetch();
-    }
+	}
+
+	public function getPropertyArticles() {
+		$model = new \Frontender\Platform\Model\SCR\Article\SearchModel( $this->container );
+		$model->setState( [
+			'must'    => [
+				[
+					'type' => 'person',
+					'id'   => $this['_id']
+				]
+			],
+			'mustNot' => [
+				'type' => 'label',
+				'id'   => 'f47307e1-8703-4759-9b99-1f0c46cadc63'
+			],
+			'limit'   => $this->getState()->articleLimit
+		] );
+
+		return $model->fetch();
+	}
+
+	public function getPropertyPublications() {
+		$model = new \Frontender\Platform\Model\SCR\Article\SearchModel( $this->container );
+		$model->setState( [
+			'must' => [
+				[
+					'type' => 'person',
+					'id'   => $this['_id']
+				],
+				[
+					'type'  => 'field',
+					'id'    => 'articleType',
+					'value' => 'issue'
+				],
+				[
+					'type' => 'label',
+					'id'   => 'f47307e1-8703-4759-9b99-1f0c46cadc63'
+				]
+			]
+		] );
+
+		return $model->fetch();
+	}
 }
